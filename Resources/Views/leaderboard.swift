37321e42bfa885fb2dc19f19e1b07f9fc6ae0875
Jeremy Greenwood <jeremy@mobelux.com>
2020-03-17 06:20:18 -0400

    updated leaf files

---
+++ Resources/Views/leaderboard.swift
@@ -0,0 +1,15 @@
+<!--
+Leaderboard Template
+
+Context:
+
+- total_count_users - Array of LeaderboardUsers
+- total_duration_users - Array of LeaderboardUsers
+
+LeaderboardUser
+
+- id
+- name
+- total_workout_count
+- total_workout_duration
+-->
