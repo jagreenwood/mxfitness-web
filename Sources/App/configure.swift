0fd04492081df04ec9c2eb82c2be59f75c1c7bde
Jeremy Greenwood <jeremy@mobelux.com>
2020-03-19 05:22:09 -0400

    added public folder

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,9 +5,6 @@ import Leaf
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         username: Environment.get("DATABASE_USERNAME") ?? "postgres",
@@ -15,6 +12,7 @@ public func configure(_ app: Application) throws {
         database: Environment.get("DATABASE_NAME") ?? "mxfitness"
     ), as: .psql)
 
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.middleware.use(SessionsMiddleware(session: app.sessions.driver))
     app.views.use(.leaf)
 
