8c438061ee7da1ff16f0296f7bc89b40a2e43092
Jeremy Greenwood <jeremy@mobelux.com>
2020-04-07 07:10:35 -0400

    style tweaks

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -23,7 +23,6 @@ public func configure(_ app: Application) throws {
     }
 
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    print(app.directory.publicDirectory)
     app.middleware.use(SessionsMiddleware(session: app.sessions.driver))
     app.views.use(.leaf)
 
